b3cddf438d743c180829621b2dcaf5a0f6b40f1c
EC2 Default User <ec2-user@ip-172-31-35-221.ap-northeast-1.compute.internal>
2025-03-25 03:48:46 +0000

    0325デブロイ完了

--- config/application.rb
+++ config/application.rb
@@ -2,14 +2,23 @@ require_relative "boot"
 
 require "rails/all"
 
+require 'dotenv/load' if Rails.env.development? || Rails.env.test?
+
 # Require the gems listed in Gemfile, including any gems
+#
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-Dotenv::Railtie.load
+
+# Load environment variables from .env file
+
 module WPA
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.2
+
+    # Set secret_key_base for production environment using Rails credentials
+    #config.secret_key_base = Rails.application.credentials.production[:secret_key_base]
+
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
