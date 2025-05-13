3141f9b5531e94df8470fcee022f4e43e3931d1a
agehamashiro <agehamashiro@gmail.com>
2025-05-13 14:00:12 +0900

    ドメイン名変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get "wines/new"
   get "wines/create"
   get "wines/show"
+  get 'www.winepair.jp', to: redirect('https://winepair.jp')
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
