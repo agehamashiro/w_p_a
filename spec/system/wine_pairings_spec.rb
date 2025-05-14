bbcdf900014cc48bdf04894aa2e20847b200cf8b
agehamashiro <agehamashiro@gmail.com>
2025-05-14 15:43:34 +0900

    Rpec

--- spec/system/wine_pairings_spec.rb
+++ spec/system/wine_pairings_spec.rb
@@ -4,7 +4,10 @@ require 'rails_helper'
 RSpec.describe "WinePairings", type: :system do
   before do
     # Gemini APIのスタブ（実通信を防ぐ）
-    allow(Net::HTTP).to receive(:new).and_return(instance_double(Net::HTTP, use_ssl=:true, request: mock_response))
+    http_double = instance_double(Net::HTTP)
+    allow(Net::HTTP).to receive(:new).and_return(http_double)
+    allow(http_double).to receive(:use_ssl=).with(true)
+    allow(http_double).to receive(:request).and_return(mock_response)
   end
 
   let(:mock_response) do
@@ -26,7 +29,7 @@ RSpec.describe "WinePairings", type: :system do
   it "ユーザーがワイン情報を入力し、提案を確認できる" do
     visit new_wine_path
 
-    select "1000～2000円", from: "ワインの価格帯"  # 修正
+    select "1000～2000円", from: "ワインの価格帯"
     fill_in "産地", with: "フランス"
     fill_in "品種", with: "ピノ・ノワール"
     fill_in "料理の好み", with: "さっぱり"
