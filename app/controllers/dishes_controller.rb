5e34c65d97502f194d2f6a7074fd9ccfc05b8bd8
agehamashiro <agehamashiro@gmail.com>
2025-06-17 14:03:57 +0900

    動的OGP3

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -1,4 +1,6 @@
 class DishesController < ApplicationController
+  before_action :set_dish_map, only: [:show, :ogp]
+
   def show
     @dish = Dish.find(params[:id])
     # 通常のHTMLレンダリング（OGPを含むheadタグも含めて表示）
@@ -16,5 +18,11 @@ class DishesController < ApplicationController
     dish_data = params.require(:dish_data).permit(:name, :description, :image_url)
     dish = Dish.create!(name: dish_data[:name], description: dish_data[:description], image_url: dish_data[:image_url])
     redirect_to dish_path(dish)
+  end
+
+  private
+
+  def set_dish_map
+    @dish_map = Dish.all.index_by(&:name)
   end
 end
