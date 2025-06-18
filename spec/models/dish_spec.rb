9ce7d0664c0767401735ba69c456d1faa68a2d81
agehamashiro <agehamashiro@gmail.com>
2025-06-18 10:31:59 +0900

    CI修正

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -5,7 +5,8 @@ RSpec.describe "Dishes", type: :request do
     it "returns http success" do
       dish = Dish.create!(
         name: "Test Dish",
-        description: "Sample description"
+        description: "Sample description",
+        image_url: "http://example.com/test.jpg"
       )
       get dish_path(dish)
       expect(response).to have_http_status(:success)
