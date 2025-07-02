46eec1565dc17c15719828c319254ec55dc0ba7b
agehamashiro <agehamashiro@gmail.com>
2025-07-02 18:48:15 +0900

    googleログイン修正

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,11 +1,16 @@
 
-
+OmniAuth.config.allowed_request_methods = [ :get, :post ]
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2,
            ENV["GOOGLE_CLIENT_ID"],
            ENV["GOOGLE_CLIENT_SECRET"],
            {
-             scope: "email,profile",
-             prompt: "select_account"
+            scope: "email,profile",
+            prompt: "select_account",
+            setup: lambda { |env|
+              request = Rack::Request.new(env)
+              Rails.logger.info "[DEBUG] redirect_uri: #{request.base_url}/auth/google_oauth2/callback"
+            }
+             
            }
 end
