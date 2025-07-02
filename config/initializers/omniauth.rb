9ea7373716be5f47fcd989fa1b7b31a882933db9
agehamashiro <agehamashiro@gmail.com>
2025-07-02 18:34:33 +0900

    googleログイン修正

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 
-OmniAuth.config.path_prefix = "/auth"
+OmniAuth.config.allowed_request_methods = [ :get, :post ]
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2,
            ENV["GOOGLE_CLIENT_ID"],
