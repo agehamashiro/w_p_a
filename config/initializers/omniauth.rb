6e9b79116096adb66a212fcdecdda34f25d63069
agehamashiro <agehamashiro@gmail.com>
2025-07-02 20:06:54 +0900

    googleログイン修正(get→post)

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -7,7 +7,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
            {
             scope: "email,profile",
             prompt: "select_account",
-            access_type: 'online',  # ← ここにカンマを追加
+            access_type: 'online',  
             setup: lambda { |env|
               request = Rack::Request.new(env)
               Rails.logger.info "[DEBUG] redirect_uri: #{request.base_url}/auth/google_oauth2/callback"
