2c15af7f777482e308dc4cc217fbf37b8265527a
agehamashiro <agehamashiro@gmail.com>
2025-07-02 19:25:55 +0900

    googleログイン修正(get→post)

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 Rails.application.config.session_store :cookie_store,
-  key: '_wpa_session',
-  secure: true,
-  same_site: :none
-end
+  key: '_wpa_session',                
+  secure: Rails.env.production?,     
+  same_site: :none,                   
+  httponly: true    
